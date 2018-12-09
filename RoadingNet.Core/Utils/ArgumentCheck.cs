4c07b58d0d8675ffdc69a54d7d009ca8f562e9ee
曹瑞鹏 <caoruipeng@outlook.com>
2018-12-09 22:18:43 +0800

    参数校验

---
+++ RoadingNet.Core/Utils/ArgumentCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoadingNet.Utils
+{
+    public class ArgumentCheck
+    {
+        /// <summary>
+        /// 检验参数不能为空，如果为空抛出异常
+        /// </summary>
+        /// <param name="argumentName"></param>
+        public static void NotNull(string argumentName)
+        {
+            if (argumentName == null)
+            {
+                throw new ArgumentException($"参数{argumentName}不能为空！",argumentName);
+            }
+        }
+    }
+}
